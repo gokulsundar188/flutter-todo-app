8ac4beca838c3d8eee4f323af8fbc4d5a6ef8877
Gokul Sundaramoorthy <gokulsundar188@gmail.com>
2026-01-11 14:11:54 +0530

    todo app code added

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,190 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const TodoApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TodoApp extends StatelessWidget {
+  const TodoApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Todo App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      home: const TodoHome(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class TodoHome extends StatefulWidget {
+  const TodoHome({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<TodoHome> createState() => _TodoHomeState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TodoHomeState extends State<TodoHome> {
+  final List<String> _todoList = [];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  final _todoController = TextEditingController();
+  final _todoFocusNode = FocusNode();
+  final _formKey = GlobalKey<FormState>();
+
+  int? _editIndex;
+
+  @override
+  void dispose() {
+    _todoController.dispose();
+    _todoFocusNode.dispose();
+
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        backgroundColor: Theme.of(context).secondaryHeaderColor,
+        elevation: 2,
+        title: Text("Todos", style: TextStyle(fontSize: 24, fontWeight: .bold)),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
+          mainAxisSize: .min,
+          spacing: 10,
+          crossAxisAlignment: .center,
+          mainAxisAlignment: .start,
           children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Row(
+              mainAxisSize: .min,
+              spacing: 10,
+              children: [
+                Expanded(
+                  child: Form(
+                    key: _formKey,
+                    child: TextFormField(
+                      controller: _todoController,
+                      focusNode: _todoFocusNode,
+
+                      validator: (val) {
+                        if (val == null || val.trim().isEmpty) {
+                          return "Todo can't be empty";
+                        }
+
+                        return null;
+                      },
+
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.grey[100],
+                        hintText: "Add todo",
+
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.blueGrey[100]!),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+
+                ElevatedButton(
+                  onPressed: _onSave,
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(80, 40),
+                    backgroundColor: Theme.of(context).secondaryHeaderColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: Text(_editIndex == null ? "Add todo" : "Update todo"),
+                ),
+              ],
+            ),
+
+            Divider(),
+
+            Expanded(
+              child: _todoList.isEmpty
+                  ? Center(
+                      child: Text(
+                        "No todos",
+                        style: TextStyle(fontSize: 18, fontWeight: .bold),
+                      ),
+                    )
+                  : ListView.builder(
+                      itemCount: _todoList.length,
+                      itemBuilder: (context, index) {
+                        var task = _todoList[index];
+
+                        return ListTile(
+                          title: Text(
+                            task,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+
+                          trailing: Row(
+                            mainAxisSize: .min,
+                            spacing: 10,
+                            children: [
+                              IconButton(
+                                onPressed: () => _onEdit(index),
+                                color: Colors.amber,
+                                icon: Icon(Icons.edit, size: 20),
+                              ),
+                              IconButton(
+                                onPressed: () => _onDelete(index),
+                                color: Colors.red,
+                                icon: Icon(Icons.delete, size: 20),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
+  }
+
+  //   events
+  void _onSave() {
+    if (!(_formKey.currentState?.validate() ?? false)) return;
+
+    setState(() {
+      var task = _todoController.text.trim();
+
+      if (_editIndex != null) {
+        _todoList[_editIndex!] = task;
+        _editIndex = null;
+      } else {
+        _todoList.add(task);
+      }
+
+      _todoController.clear();
+      _todoFocusNode.unfocus();
+    });
+  }
+
+  void _onEdit(int index) {
+    setState(() {
+      _editIndex = index;
+      _todoController.text = _todoList[index];
+
+      _todoFocusNode.requestFocus();
+    });
+  }
+
+  void _onDelete(int index) {
+    setState(() => _todoList.removeAt(index));
   }
 }
